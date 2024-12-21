92f55d911a8a8c27ee9a1546a2317e35ceccef89
Coder3609 <d2424181@gmail.com>
2024-12-21 12:02:58 +0530

    Added new features to todo List

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css'
-
+import TodoList from './TodoList'
 function App() {
   
 
   return (
     <>
-      
+      <TodoList />
     </>
   )
 }
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,8 +1,38 @@
+import {useState} from "react";
+import {v4 as uuidv4} from 'uuid';
+
+
 export default function TodoList(){
+    let[todos,setTodos]=useState([{task:"sample task" , id:uuidv4()}]);
+    let [newTodo,setNewTodo]= useState("")
+
+    let addNewTask = () =>{
+        setTodos([...todos, newTodo])
+        setNewTodo("");
+    }
+
+    let updateTodoValue = (event) => {
+        setNewTodo(event.target.value)
+    }
+
    return(
     <div>
-        <input></input>
-        <h4>Todo List</h4>
+        <input placeholder="add a task" value={newTodo} onChange={updateTodoValue}></input>
+        <button onClick={addNewTask}>Add Task</button>
+        <br></br>
+        <br></br>
+        <br></br>
+
+        <hr></hr>
+
+        <h4>Tasks Todo</h4>
+        <ul>
+            {todos.map((todo) => (
+                <li key={todo.id}>{todo.task}</li>
+            ))
+
+            }
+        </ul>
     </div>
    )
 }
